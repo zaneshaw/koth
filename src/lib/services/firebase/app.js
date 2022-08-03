b6a4da76d1649972e4f6dea35064a8973abfe5a6
Zane Shaw <zaneshaw.home@gmail.com>
2022-08-03 17:44:46 +1000

    Add firebase

---
+++ src/lib/services/firebase/app.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCo_EXnjPO8ieIe0SSLIvvCj8Oxas_O35w",
+    authDomain: "koth-b8d65.firebaseapp.com",
+    projectId: "koth-b8d65",
+    storageBucket: "koth-b8d65.appspot.com",
+    messagingSenderId: "915492719236",
+    appId: "1:915492719236:web:fe7b3d0aa32a95eae150d1",
+    measurementId: "G-V46L9LEFGL",
+};
+
+export const app = initializeApp(firebaseConfig);
