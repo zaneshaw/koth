95279d4273bcbab3b36e9ae2d8cd2680ce3bbfcb
Zane Shaw <zaneshaw.home@gmail.com>
2022-08-03 12:53:31 +1000

    Add skeleton

--- src/App.svelte
+++ src/App.svelte
@@ -1,48 +1,29 @@
 <script>
-    import svelteLogo from "./assets/svelte.svg";
-    import Counter from "./lib/Counter.svelte";
+    let king = "squidee_";
+    let kingTime = "4 hours";
+
+    const beKing = function() {
+        console.log("asdasd");
+    }
 </script>
 
-<main>
+<main class="flex flex-col justify-between mx-auto min-h-screen text-center">
+    <div class="mt-6">
+        <h3>triangle100's</h3>
+        <h2 class="font-semibold">king of the hill</h2>
+    </div>
     <div>
-        <a href="https://vitejs.dev" target="_blank">
-            <img src="/vite.svg" class="logo" alt="Vite Logo" />
-        </a>
-        <a href="https://svelte.dev" target="_blank">
-            <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-        </a>
+        <h1><span class="font-semibold">{king}</span> is the current king</h1>
+        <p>and has been for <span class="font-semibold">{kingTime}</span></p>
+
+        <button on:click={beKing} class="my-8">become the king</button>
     </div>
-    <h1>Vite + Svelte</h1>
-
-    <div class="card">
-        <Counter />
+    <div>
+        <p>
+            check out the <a
+                href="https://github.com/sveltejs/kit#readme"
+                target="_blank">source</a
+            > for this project
+        </p>
     </div>
-
-    <p>
-        Check out <a
-            href="https://github.com/sveltejs/kit#readme"
-            target="_blank">SvelteKit</a
-        >, the official Svelte app framework powered by Vite!
-    </p>
-
-    <p class="read-the-docs">
-        Click on the Vite and Svelte logos to learn more
-    </p>
 </main>
-
-<style>
-    .logo {
-        height: 6em;
-        padding: 1.5em;
-        will-change: filter;
-    }
-    .logo:hover {
-        filter: drop-shadow(0 0 2em #646cffaa);
-    }
-    .logo.svelte:hover {
-        filter: drop-shadow(0 0 2em #ff3e00aa);
-    }
-    .read-the-docs {
-        color: #888;
-    }
-</style>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,10 @@ const config = {
   content: ["./src/**/*.{html,js,svelte,ts}"],
 
   theme: {
-    extend: {},
+    fontFamily: {
+        "sans": ['"Montserrat"'],
+        "mono": ['monospace'],
+    },
   },
 
   plugins: [],
